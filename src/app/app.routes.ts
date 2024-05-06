0e9a6c74a584bc237b7980e4f6f22a833c9dab7d
ShrewdLuni <ander113q@gmail.com>
2024-05-07 00:01:59 +0300

    feat: 404 page & app router

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { MainComponent } from './main/main.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'first-component', component: MainComponent },
+    { path: '**', component: PageNotFoundComponent },
+];
