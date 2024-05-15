1dac228a3e8c7c0ddf3804c03c24bb0ab72aa4f1
ShrewdLuni <ander113q@gmail.com>
2024-05-16 00:07:59 +0300

    feat: add dashboard template

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { CreateComponent } from './features/create/create.component';
 import { ErrorComponent } from './features/error/error.component';
+import { DashboardComponent } from './features/dashboard/dashboard.component';
 
 export const routes: Routes = [
     { path: '', component: CreateComponent },
+    { path: 'dashboard', component: DashboardComponent },
     { path: '**', component: ErrorComponent },//fix: router
 ];
