783fcff173bb8c9eddc3dc0e41d17324e0918ff6
ShrewdLuni <ander113q@gmail.com>
2024-05-17 21:08:39 +0300

    feat: add payment component template

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { CreateComponent } from './features/create/create.component';
 import { ErrorComponent } from './features/error/error.component';
 import { DashboardComponent } from './features/dashboard/dashboard.component';
+import { PaymentComponent } from './features/payment/payment.component';
 
 export const routes: Routes = [
     { path: '', component: CreateComponent },
     { path: 'dashboard', component: DashboardComponent },
+    { path: 'payment', component: PaymentComponent },
     { path: '**', component: ErrorComponent },//fix: router
 ];
---
+++ src/app/features/payment/payment.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-payment',
+    standalone: true,
+    templateUrl: './payment.component.html',
+})
+export class PaymentComponent {
+  title = 'FileForMoney';
+}
